c7ce6e2905cc2def4dcbb242ceb15add858a0516
duynhan935 <tranduynhan2005ct@gmail.com>
2025-01-16 23:59:20 +0700

    Demo cho header

--- src/components/layout/client/app.layout.jsx
+++ src/components/layout/client/app.layout.jsx
@@ -1,16 +1,16 @@
-import { Outlet } from "react-router-dom"
-import AppHeader from "./header/app.header"
-import AppFooter from "./footer/app.footer"
-
+import { Outlet } from "react-router-dom";
+import AppHeader from "./header/app.header";
+import AppFooter from "./footer/app.footer";
+import "../../../assets/style/global.css";
 
 const AppLayout = () => {
     return (
         <div className="layout">
             <AppHeader />
-            <Outlet />
-            <AppFooter />
+            {/* <Outlet />
+            <AppFooter /> */}
         </div>
-    )
-}
+    );
+};
 
-export default AppLayout
+export default AppLayout;
--- src/components/layout/client/header/app.header.jsx
+++ src/components/layout/client/header/app.header.jsx
@@ -1,8 +1,58 @@
+import React from "react";
+import { Menu, Badge, Avatar } from "antd";
+import { ShoppingCartOutlined } from "@ant-design/icons";
+import "../../../../assets/style/app.header.css";
+import "../../../../assets/style/global.css";
 
 const AppHeader = () => {
     return (
-        <div>AppHeader</div>
-    )
-}
+        <header class="header">
+            <div class="container">
+                <div class="header-top">
+                    <div class="logo">
+                        <img src="" alt="logo" class="logo__img" />
+                    </div>
+                    <nav class="navbar">
+                        <ul class="navbar__list">
+                            <li class="navbar__item">
+                                <a
+                                    href="#!"
+                                    class="navbar__link navbar__link--active"
+                                >
+                                    Home
+                                </a>
+                            </li>
+                            <li class="navbar__item">
+                                <a href="#!" class="navbar__link">
+                                    Cart
+                                </a>
+                            </li>
+                        </ul>
+                    </nav>
+                    <div class="header-action">
+                        <div className="shopping-cart">
+                            <a href="#!" class="shopping-cart__link">
+                                <ShoppingCartOutlined
+                                    style={{
+                                        fontSize: "2.4rem",
+                                        color: "#000",
+                                    }}
+                                />
+                                <span className="cart-number">0</span>
+                            </a>
+                        </div>
+                        <a href="#!" class="header-action__link">
+                            Sign in
+                        </a>
 
-export default AppHeader
+                        <a href="#!" class="header-action__sign-up btn">
+                            Sign up
+                        </a>
+                    </div>
+                </div>
+            </div>
+        </header>
+    );
+};
+
+export default AppHeader;
