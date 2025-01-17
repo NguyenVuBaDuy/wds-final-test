7dbe0d9deddfeb91b4946b21b71a3964f113782b
duynhan935 <tranduynhan2005ct@gmail.com>
2025-01-17 08:09:06 +0700

    Header demo 2

--- src/components/layout/client/header/app.header.jsx
+++ src/components/layout/client/header/app.header.jsx
@@ -1,8 +1,106 @@
 import React from "react";
-import { Menu, Badge, Avatar } from "antd";
-import { ShoppingCartOutlined } from "@ant-design/icons";
+import { Menu, Badge, Avatar, Space, Dropdown } from "antd";
+import {
+    ShoppingCartOutlined,
+    UserOutlined,
+    DownOutlined,
+    LogoutOutlined,
+    ProfileOutlined,
+    SettingOutlined,
+} from "@ant-design/icons";
 import "../../../../assets/style/app.header.css";
 import "../../../../assets/style/global.css";
+
+const items = [
+    {
+        key: "1",
+        label: (
+            <a
+                href="/profile"
+                style={{ display: "flex", alignItems: "center" }}
+            >
+                <ProfileOutlined style={{ marginRight: "8px" }} />
+                Profile
+            </a>
+        ),
+    },
+    {
+        key: "2",
+        label: (
+            <a
+                href="/settings"
+                style={{ display: "flex", alignItems: "center" }}
+            >
+                <SettingOutlined style={{ marginRight: "8px" }} />
+                Settings
+            </a>
+        ),
+    },
+    {
+        type: "divider",
+    },
+    {
+        key: "3",
+        label: (
+            <a
+                href="/logout"
+                style={{ display: "flex", alignItems: "center", color: "red" }}
+            >
+                <LogoutOutlined style={{ marginRight: "8px" }} />
+                Logout
+            </a>
+        ),
+    },
+];
+
+const CheckLogin = () => {
+    const isAuthentication = true;
+
+    return (
+        <>
+            {isAuthentication ? (
+                <Dropdown
+                    menu={{ items }}
+                    trigger={["click"]}
+                    overlayStyle={{ minWidth: "200px" }}
+                >
+                    <div
+                        onClick={(e) => e.preventDefault()}
+                        style={{
+                            position: "relative",
+                            display: "inline-block",
+                            cursor: "pointer",
+                        }}
+                    >
+                        <Avatar size="large" icon={<UserOutlined />} />
+                        <DownOutlined
+                            style={{
+                                position: "absolute",
+                                bottom: "-2px",
+                                right: "-2px",
+                                background: "#252728",
+                                borderRadius: "50%",
+                                padding: "2px",
+                                fontSize: "10px",
+                                color: "#fff",
+                                boxShadow: "0 0 2px rgba(0,0,0,0.2)",
+                            }}
+                        />
+                    </div>
+                </Dropdown>
+            ) : (
+                <>
+                    <a href="#!" className="header-action__link">
+                        Sign in
+                    </a>
+                    <a href="#!" className="header-action__sign-up btn">
+                        Sign up
+                    </a>
+                </>
+            )}
+        </>
+    );
+};
 
 const AppHeader = () => {
     return (
@@ -41,13 +139,7 @@ const AppHeader = () => {
                                 <span className="cart-number">0</span>
                             </a>
                         </div>
-                        <a href="#!" class="header-action__link">
-                            Sign in
-                        </a>
-
-                        <a href="#!" class="header-action__sign-up btn">
-                            Sign up
-                        </a>
+                        <CheckLogin />
                     </div>
                 </div>
             </div>
