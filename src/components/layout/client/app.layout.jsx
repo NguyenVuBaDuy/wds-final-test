babba73ff704126f2d49d29ffbde82cc738d14ca
Ba Duy <nguyenvubaduy9a11a@gmail.com>
2025-01-19 07:13:09 +0700

    fix: change component Product to Outlet in app.layout

--- src/components/layout/client/app.layout.jsx
+++ src/components/layout/client/app.layout.jsx
@@ -1,19 +1,12 @@
 import { Outlet } from "react-router-dom";
 import AppHeader from "./header/app.header";
 import AppFooter from "./footer/app.footer";
-import Home from "../../client/home/home";
-import Product from "../../client/product/product";
-import RelatedProducts from "../../client/product/related-product";
 import "../../../assets/style/global.css";
-import History from "../../client/history/history";
-import Order from "../../client/order/order";
 const AppLayout = () => {
     return (
         <div className="layout">
             <AppHeader />
-
-            <Product/>
-
+            <Outlet />
             <AppFooter />
         </div>
     );
