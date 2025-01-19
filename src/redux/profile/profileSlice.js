662cf0a78c51ed1f63d26bf8d37bb04bfc12bb99
Ba Duy <nguyenvubaduy9a11a@gmail.com>
2025-01-19 20:26:22 +0700

    call api to login and save profile user into redux, custom button login, register

---
+++ src/redux/profile/profileSlice.js
@@ -0,0 +1,35 @@
+import { createSlice } from '@reduxjs/toolkit';
+
+
+const initialState = {
+    isAuthenticated: false,
+    tempAvatar: null,
+    user: {
+        id: '',
+        email: '',
+        phone_number: '',
+        name: '',
+        role: '',
+        avatar: '',
+    }
+};
+
+const profileSlice = createSlice({
+    name: 'account',
+    initialState,
+    reducers: {
+        doGetProfileAction: (state, action) => {
+            state.isAuthenticated = true
+            state.user = action.payload
+        },
+        doLogoutAction: (state) => {
+            state.isAuthenticated = false
+            state.user = initialState.user
+            localStorage.removeItem('access_token')
+        }
+    },
+});
+
+export const { doGetProfileAction, doLogoutAction } = profileSlice.actions;
+
+export default profileSlice.reducer;
--- src/services/api.service.js
+++ src/services/api.service.js
@@ -11,6 +11,22 @@ const registerAPI = (name, email, phone_number, password) => {
     return axios.post(urlBackend, data)
 }
 
+const loginAPI = (email, password) => {
+    const urlBackend = '/auth/login'
+    const data = {
+        email,
+        password
+    }
+    return axios.post(urlBackend, data)
+}
+
+const getProfileAPI = () => {
+    const urlBackend = '/auth/profile'
+    return axios.get(urlBackend)
+}
+
 export {
-    registerAPI
+    registerAPI,
+    loginAPI,
+    getProfileAPI
 }
